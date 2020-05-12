be4c7c70d9d3f44fe1818ecd02489bde2845bd67
samuelcarvalhodev <samuelmacal.2015@gmail.com>
2020-05-11 22:43:56 -0300

    front end app

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,12 +1,20 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(response, request){
-        const ong_id = request.headers.authorization;
-        const incidents = await connection('incidents')
-            .where('ong_id', ong_id)
-            .select('*');
+    async index(request, response){
+        try{        
+            
+            const ong_id = request.headers.authorization;
+            
+            const incidents = await connection('incidents')
+                .where('ong_id', ong_id)
+                .select('*');
+
             return response.json(incidents);
+        }catch(err){
+            return response.status(404).json({ error: 'Error when listing ONG incidents' });
+        }
+            
     }
 
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,15 +9,18 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 //Rota de inserir dados na table Incidents
-routes.post('/incidents', IncidentController.create);
-//Listar os dados da table Incidents
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete );
+
 
 //Listar os dados da table Ongs
 routes.get('/ongs', OngController.index);
 //Rota de inserir dados na table Ongs
 routes.post('/ongs', OngController.create);
+
+routes.post('/incidents', IncidentController.create);
+//Listar os dados da table Incidents
+
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete );
 
 
 routes.get('/profile', ProfileController.index);
